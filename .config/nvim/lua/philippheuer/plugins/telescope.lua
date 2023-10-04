9db5c4bb02873daa36c1b8bbba53f8aa34954348
Philipp Heuer <git@philippheuer.me>
2023-10-04 22:34:00 +0200

    feat(nvim): add trouble plugin and update mapping

--- .config/nvim/lua/philippheuer/plugins/telescope.lua
+++ .config/nvim/lua/philippheuer/plugins/telescope.lua
@@ -4,14 +4,10 @@ return {
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
     dependencies = {
+      -- required
       'nvim-lua/plenary.nvim',
-      -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-      -- Only load if `make` is available. Make sure you have the system
-      -- requirements installed.
       {
         'nvim-telescope/telescope-fzf-native.nvim',
-        -- NOTE: If you are having trouble with this installation,
-        --       refer to the README for telescope-fzf-native for more instructions.
         build = 'make',
         cond = function()
           return vim.fn.executable 'make' == 1
