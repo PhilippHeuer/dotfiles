501777212ffc0d410209cd1ac30b71788682393e
Philipp Heuer <git@philippheuer.me>
2023-10-05 02:42:03 +0200

    refactor(nvim): simplify plugin configuration

--- .config/nvim/lua/philippheuer/plugins/whichkey.lua
+++ .config/nvim/lua/philippheuer/plugins/whichkey.lua
@@ -8,6 +8,19 @@ return {
         separator = "➜ ", -- symbol used between a key and it's label
         group = "+" -- symbol prepended to a group
       }
-    }
+    },
+    config = function()
+      -- [[ document command categories ]]
+      require('which-key').register({
+        ['<leader>u'] = { name = '[U]I', _ = 'which_key_ignore' },
+        ['<leader>h'] = { name = '[H]arpoon', _ = 'which_key_ignore' },
+        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+        ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
+        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+      })
+    end,
   },
 }
