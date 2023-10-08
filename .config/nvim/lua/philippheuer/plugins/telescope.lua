8b9c441c7fc0f4c608dada49b34e1dca015d0e58
Philipp Heuer <git@philippheuer.me>
2023-10-08 17:19:13 +0200

    feat(nvim): sk to search keymaps using telescope

--- .config/nvim/lua/philippheuer/plugins/telescope.lua
+++ .config/nvim/lua/philippheuer/plugins/telescope.lua
@@ -63,6 +63,7 @@ return {
       vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
+      vim.keymap.set('n', '<leader>sk', require('telescope.builtin').keymaps, { desc = '[S]earch [K]eymaps' })
     end,
   },
 }
