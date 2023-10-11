ad96ded491041984c0bde644339fd74cc27ccd4f
Philipp Heuer <git@philippheuer.me>
2023-10-11 22:36:51 +0200

    feat(nvim): add custom project finder using tmx

--- .config/nvim/lua/philippheuer/plugins/telescope.lua
+++ .config/nvim/lua/philippheuer/plugins/telescope.lua
@@ -1,57 +1,57 @@
 return {
   -- Fuzzy Finder (files, lsp, etc)
   {
-    'nvim-telescope/telescope.nvim',
-    branch = '0.1.x',
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
     dependencies = {
       -- required
-      'nvim-lua/plenary.nvim',
+      "nvim-lua/plenary.nvim",
       {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'make',
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
         cond = function()
-          return vim.fn.executable 'make' == 1
+          return vim.fn.executable "make" == 1
         end,
       },
     },
     config = function()
       local utils = require("philippheuer.utils")
 
-      require('telescope').setup {
+      require("telescope").setup {
         defaults = {
           mappings = {
             i = {
-              ['<C-u>'] = false,
-              ['<C-d>'] = false,
+              ["<C-u>"] = false,
+              ["<C-d>"] = false,
             },
           },
         },
       }
 
       -- Enable telescope fzf native, if installed
-      pcall(require('telescope').load_extension, 'fzf')
+      pcall(require("telescope").load_extension, "fzf")
 
       -- See `:help telescope.builtin`
-      vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[S]earch [B]uffers' })
-      vim.keymap.set('n', '<leader>sb', require('telescope.builtin').buffers, { desc = '[S]earch [B]uffers' })
-      vim.keymap.set('n', '<leader>sc', function()
-        require('telescope.builtin').current_buffer_fuzzy_find({
-          sorting_strategy = 'ascending',
-          layout_strategy = 'horizontal',
+      vim.keymap.set("n", "<leader><space>", require("telescope.builtin").buffers, { desc = "[S]earch [B]uffers" })
+      vim.keymap.set("n", "<leader>sb", require("telescope.builtin").buffers, { desc = "[S]earch [B]uffers" })
+      vim.keymap.set("n", "<leader>sc", function()
+        require("telescope.builtin").current_buffer_fuzzy_find({
+          sorting_strategy = "ascending",
+          layout_strategy = "horizontal",
           layout_config = {
             preview_width = 0.5,
           },
         })
-      end, { desc = '[S]earch [c]urrent buffer' })
-      vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-      vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-      vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-      vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>sk', require('telescope.builtin').keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sr', require('telescope.builtin').oldfiles, { desc = '[S]earch [r]ecent files' })
-      vim.keymap.set('n', '<leader>sgf', function()
+      end, { desc = "[S]earch [c]urrent buffer" })
+      vim.keymap.set("n", "<leader>sf", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
+      vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
+      vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
+      vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
+      vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
+      vim.keymap.set("n", "<leader>sr", require("telescope.builtin").resume, { desc = "[S]earch [R]esume" })
+      vim.keymap.set("n", "<leader>sk", require("telescope.builtin").keymaps, { desc = "[S]earch [K]eymaps" })
+      vim.keymap.set("n", "<leader>sr", require("telescope.builtin").oldfiles, { desc = "[S]earch [r]ecent files" })
+      vim.keymap.set("n", "<leader>sgf", function()
         local cmd = {
           "sort",
           "-u",
@@ -65,7 +65,43 @@ return {
         else
           require("telescope.builtin").git_files()
         end
-      end, { desc = '[S]earch [G]it [F]iles' })
+      end, { desc = "[S]earch [G]it [F]iles" })
+
+      -- telescope picker - tmx projects
+      project_picker = function()
+        local output = vim.fn.system("tmx project --mode telescope")
+        if vim.v.shell_error ~= 0 then
+          print("Error executing command: " .. output)
+          return
+        end
+        local results = vim.fn.json_decode(output)
+
+        local finders = require("telescope.finders")
+        local sorters = require("telescope.sorters")
+        local action_state = require("telescope.actions.state")
+
+        return require("telescope.pickers").new {
+          prompt_title = "Search for Projects",
+          finder = finders.new_table{
+            results = results,
+            entry_maker = function (x) return x end,
+          },
+          sorter = sorters.fuzzy_with_index_bias(),
+          selection_strategy = "row",
+          attach_mappings = function(prompt_bufnr, map)
+            -- Map the enter key to run the command on the selected item
+            map("i", "<CR>", function()
+              local selection = action_state.get_selected_entry()
+              require("telescope.actions").close(prompt_bufnr)
+              os.execute(string.format("tmx project --select \"%s\"", selection.value))
+            end)
+
+            return true
+          end,
+        }:find()
+      end
+      vim.keymap.set("n", "<leader>sp", project_picker, { desc = "[S]earch [P]roject" })
+
     end,
   },
 }
