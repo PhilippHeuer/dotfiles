1e726f9fb67c1e5220795f57842c74eaad9d7346
Philipp Heuer <git@philippheuer.me>
2023-10-13 22:16:23 +0200

    perf(nvim): load lspconfig on BufReadPre and BufNewFile

--- .config/nvim/lua/philippheuer/plugins/lspconfig.lua
+++ .config/nvim/lua/philippheuer/plugins/lspconfig.lua
@@ -3,30 +3,31 @@ return {
   --  The configuration is done below. Search for lspconfig to find it below.
   {
     -- LSP Configuration & Plugins
-    'neovim/nvim-lspconfig',
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
     dependencies = {
       -- Automatically install LSPs to stdpath for neovim
-      { 'williamboman/mason.nvim', config = true },
-      'williamboman/mason-lspconfig.nvim',
+      { "williamboman/mason.nvim", config = true },
+      "williamboman/mason-lspconfig.nvim",
 
       -- json and yaml schema store
-      { 'b0o/schemastore.nvim' },
+      { "b0o/schemastore.nvim" },
 
       -- Useful status updates for LSP
-      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
+      -- NOTE: `opts = {}` is the same as calling `require("fidget").setup({})`
+      { "j-hui/fidget.nvim", tag = "legacy", opts = {} },
 
       -- Additional lua configuration, makes nvim stuff amazing!
-      'folke/neodev.nvim',
+      "folke/neodev.nvim",
     },
   },
   {
-    'folke/trouble.nvim',
-    event = { 'BufRead' },
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    "folke/trouble.nvim",
+    event = { "BufRead" },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
       auto_open = false,
-      mode = 'workspace_diagnostics',
+      mode = "workspace_diagnostics",
     },
   },
 }
