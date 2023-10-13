c9682efc7ca89b983ae56e9169e6d732f9816a6d
Philipp Heuer <git@philippheuer.me>
2023-10-13 22:24:25 +0200

    feat(nvim): set event of nvim-cmp to InsertInter, add completion sources

--- .config/nvim/lua/philippheuer/plugins/completion.lua
+++ .config/nvim/lua/philippheuer/plugins/completion.lua
@@ -1,39 +1,48 @@
 return {
     {
       -- Autocompletion
-      'hrsh7th/nvim-cmp',
+      "hrsh7th/nvim-cmp",
+      event = "InsertEnter",
       dependencies = {
-        -- Snippet Engine & its associated nvim-cmp source
-        'L3MON4D3/LuaSnip',
-        'saadparwaiz1/cmp_luasnip',
-           -- Adds LSP completion capabilities
-        'hrsh7th/cmp-nvim-lsp',
-           -- Adds a number of user-friendly snippets
-        'rafamadriz/friendly-snippets',
+        -- snippets
+        "L3MON4D3/LuaSnip", -- snipet engine
+        "saadparwaiz1/cmp_luasnip", -- snippet engine nvim-cmp source
+        -- completion
+        "hrsh7th/cmp-nvim-lsp", -- lsp completion capabilities
+        "hrsh7th/cmp-buffer", -- buffer completion
+        "hrsh7th/cmp-path", -- file path completion
+        "rafamadriz/friendly-snippets", -- Adds a number of user-friendly snippets
       },
       config = function()
-        local cmp = require 'cmp'
-        local luasnip = require 'luasnip'
-        require('luasnip.loaders.from_vscode').lazy_load()
+        local cmp = require("cmp")
+        local luasnip = require("luasnip")
+
+        -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
+        require("luasnip.loaders.from_vscode").lazy_load()
         luasnip.config.setup {}
 
         cmp.setup {
+          completion = {
+            completeopt = "menu,menuone,preview,noselect",
+          },
+          -- configures how nvim-cmp interacts with the snippet engine
           snippet = {
             expand = function(args)
               luasnip.lsp_expand(args.body)
             end,
           },
           mapping = cmp.mapping.preset.insert {
-            ['<C-n>'] = cmp.mapping.select_next_item(),
-            ['<C-p>'] = cmp.mapping.select_prev_item(),
-            ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-            ['<C-f>'] = cmp.mapping.scroll_docs(4),
-            ['<C-Space>'] = cmp.mapping.complete {},
-            ['<CR>'] = cmp.mapping.confirm {
+            ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
+            ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+            ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+            ["<C-f>"] = cmp.mapping.scroll_docs(4),
+            ["<C-Space>"] = cmp.mapping.complete {}, -- show completion suggestions
+            ["<C-e>"] = cmp.mapping.abort(), -- close completion window
+            ["<CR>"] = cmp.mapping.confirm({
               behavior = cmp.ConfirmBehavior.Replace,
               select = true,
-            },
-            ['<Tab>'] = cmp.mapping(function(fallback)
+            }),
+            ["<Tab>"] = cmp.mapping(function(fallback)
               if cmp.visible() then
                 cmp.select_next_item()
               elseif luasnip.expand_or_locally_jumpable() then
@@ -41,8 +50,8 @@ return {
               else
                 fallback()
               end
-            end, { 'i', 's' }),
-            ['<S-Tab>'] = cmp.mapping(function(fallback)
+            end, { "i", "s" }),
+            ["<S-Tab>"] = cmp.mapping(function(fallback)
               if cmp.visible() then
                 cmp.select_prev_item()
               elseif luasnip.locally_jumpable(-1) then
@@ -50,14 +59,15 @@ return {
               else
                 fallback()
               end
-            end, { 'i', 's' }),
+            end, { "i", "s" }),
           },
           sources = {
-            { name = 'nvim_lsp' },
-            { name = 'luasnip' },
+            { name = "nvim_lsp" }, -- lsp
+            { name = "luasnip" }, -- snippets
+            { name = "buffer" }, -- text within current buffer
+            { name = "path" }, -- file path completion
           },
         }
-
       end,
     },
 }
