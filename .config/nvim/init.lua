beb5c5d7719b503fff360934a074688b37106f6d
Philipp Heuer <git@philippheuer.me>
2023-10-14 20:11:40 +0200

    feat(nvim): add mason-tool-installer for linters and formatters

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,2 @@
 -- NeoVIM Configuration
-require('philippheuer')
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+require("philippheuer")
--- .config/nvim/lua/philippheuer/lsp/gopls.lua
+++ .config/nvim/lua/philippheuer/lsp/gopls.lua
@@ -2,8 +2,8 @@ return {
   filetypes = { "go", "gomod", "gowork", "gotmpl" },
   settings = {
     gopls = {
-      completeUnimported = true,
-      usePlaceholders = true,    
+      completeUnimported = true, -- auto-import missing packages when using autocomplete
+      usePlaceholders = true, -- add placeholder params when completing a function
       analyses = {
         unusedparams = true,
       },
