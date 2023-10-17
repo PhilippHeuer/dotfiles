1f25d4decdfa475c16acb5faef8a3016c65caeb5
Philipp Heuer <git@philippheuer.me>
2023-10-17 19:55:52 +0200

    refactor: rename .config to config

--- setup.sh
+++ setup.sh
@@ -2,15 +2,15 @@
 
 # bash config
 rm -rf $HOME/.config/bash
-ln -s $(pwd)/.config/bash $HOME/.config/bash
+ln -s $(pwd)/config/bash $HOME/.config/bash
 
 # ansible config
 rm -rf $HOME/.ansible.cfg
-ln -s $(pwd)/.config/ansible/ansible.cfg $HOME/.ansible.cfg
+ln -s $(pwd)/config/ansible/ansible.cfg $HOME/.ansible.cfg
 
-# tmux config - for each file in $(pwd)/.config/tmux
+# tmux config - for each file in $(pwd)/config/tmux
 mkdir -p $HOME/.config/tmux
-for path in $(pwd)/.config/tmux/*; do
+for path in $(pwd)/config/tmux/*; do
   filename=$(basename "$path")
   rm "$HOME/.config/tmux/$filename" 2> /dev/null
   ln -s "$path" "$HOME/.config/tmux/$filename"
@@ -19,17 +19,17 @@ tmux source $HOME/.config/tmux/tmux.conf # force reload tmux config
 
 # neovim
 rm -rf $HOME/.config/nvim 2> /dev/null
-ln -s $(pwd)/.config/nvim $HOME/.config/nvim
+ln -s $(pwd)/config/nvim $HOME/.config/nvim
 
 # nushell
 rm -rf $HOME/.config/nushell
-ln -s $(pwd)/.config/nushell $HOME/.config/nushell
+ln -s $(pwd)/config/nushell $HOME/.config/nushell
 
 # starship
 rm -rf $HOME/.config/starship.toml 2> /dev/null
-ln -s $(pwd)/.config/starship.toml $HOME/.config/starship.toml
+ln -s $(pwd)/config/starship.toml $HOME/.config/starship.toml
 
 # k9s
 mkdir -p $HOME/.config/k9s
 rm -rf $HOME/.config/k9s/skin.yml
-ln -s $(pwd)/.config/k9s/skin.yml $HOME/.config/k9s/skin.yml
+ln -s $(pwd)/config/k9s/skin.yml $HOME/.config/k9s/skin.yml
