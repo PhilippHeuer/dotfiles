fd4dfc7c6ab736cedfa30afa890c1b58b38bd208
Philipp Heuer <git@philippheuer.me>
2023-11-07 00:39:17 +0100

    feat(sh): add bash aliases

---
+++ config/bash/aliases.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# diskfree
+alias df='df -h'
+
+# free memory
+alias free="free -mt"
+
+# journalctl
+alias jctl="journalctl -p 3 -xb"
+
+# use nnn as file manager
+alias l='exa --long --header'
