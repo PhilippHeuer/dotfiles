e35edc99ccf18810e71df8cea0bcefc630bf3ced
Philipp Heuer <git@philippheuer.me>
2023-11-22 00:24:28 +0100

    feat(sh): add copilot cli aliases and config

--- config/bash/aliases.sh
+++ config/bash/aliases.sh
@@ -11,3 +11,23 @@ alias jctl="journalctl -p 3 -xb"
 
 # use nnn as file manager
 alias l="exa --long --header"
+
+# gh copilot suggest
+ghc-suggest() {
+  context="Shell: $(basename $SHELL), Distro: $(source /etc/os-release && echo $NAME), Editor: $EDITOR"
+  if git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+    context="$context, Git Repo, Preferred Tools: rg"
+  fi
+
+  request="$@. $context"
+  echo "REQUEST: $request"
+  gh copilot suggest -t shell "$request"
+}
+alias ghs="ghc-suggest"
+
+# gh copilot explain
+# output: remove the first 8 lines with the banner, remove 2 leading spaces, remove trailing empty lines 
+ghc-explain() {
+  gh copilot explain "$@" | tail -n +8 | sed -E 's/^ {0,2}//' | sed -e :a -e '/^\n*$/{$d;N;ba' -e '}'
+}
+alias ghe="ghc-explain"
