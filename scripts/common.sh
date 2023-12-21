6c7f878a7248ec65d84c64ed81cd2ef696d9288b
Philipp Heuer <git@philippheuer.me>
2023-12-22 00:20:16 +0100

    feat(ansible): add swaybg, wlr-randr to sway

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
 ###
+# Global variables
+#
+# contains host-specific variables that can not be detected automatically, can be removed once wlr-randr works on wsl
+###
+export HOSTNAME_HASH=$(printf "DF/$HOSTNAME" | sha256sum  | cut -c1-64)
+if [ "$HOSTNAME_HASH" = "45d416256eed31b1e8d6221348dbd5d43f0ebcef4a7fa6848af6961c20de991c" ]; then
+    export RESOLUTION_WIDTH=3440
+    export RESOLUTION_HEIGHT=1440
+else
+    export RESOLUTION_WIDTH=1920
+    export RESOLUTION_HEIGHT=1080
+fi
+
+###
 # Checks whether the script is running in a Windows Subsystem for Linux (WSL) environment.
 #
 # Returns:
