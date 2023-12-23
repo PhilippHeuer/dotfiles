e0b8eff385dab4ff096b4bc8cfdbb0609c425f25
Philipp Heuer <git@philippheuer.me>
2023-12-23 03:40:36 +0100

    feat(nix): add activationScript to unmount /tmp/.X11-unix

--- ansible/README.md
+++
@@ -1,6 +0,0 @@
-# Ansible Playbook
-
-| Role                                       | Description              |
-| ------------------------------------------ | ------------------------ |
-| [Neovim](https://github.com/neovim/neovim) | neovim text editor       |
-| [Starship](https://starship.rs)            | starship terminal prompt |
--- nix/configuration/wsl/default.nix
+++ nix/configuration/wsl/default.nix
@@ -1,6 +1,7 @@
 {
   pkgs,
   username,
+  lib,
   ...
 }: {
   # timezone
@@ -21,13 +22,18 @@
 
     # mount options
     wslConf.automount.mountFsTab = true;
-    wslConf.automount.root = "/mnt";
+    wslConf.automount.root = "/mnt"; # also update path in unmount-x11 activation script
     wslConf.automount.options = "metadata,uid=1000,gid=1000,umask=022,fmask=111,case=off";
   };
 
   # X11 mount
-  wsl.wslgMountX11 = false;
-  systemd.tmpfiles.rules = [
-    "d /tmp/.X11-unix 1777 root root"
-  ];
+  system.activationScripts = { # wsl automatically mounts /tmp/.X11-unix
+    unmount-x11 = ''
+      # unmount /tmp/.X11-unix if it is mounted
+      umount /tmp/.X11-unix || true
+
+      # remove wslg x11 mount from systemd.tmpfiles.rules
+      ${pkgs.gnused}/bin/sed -i "\|L /tmp/.X11-unix - - - - /mnt/wslg/.X11-unix|d" /etc/tmpfiles.d/00-nixos.conf
+    '';
+  };
 }
