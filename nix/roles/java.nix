f2ce39b3029d54b2c73e2b1f0c9f6fcc5a3a92dd
Philipp Heuer <git@philippheuer.me>
2024-01-09 22:39:51 +0100

    feat(nix): generate java keystore based on cacerts

--- nix/roles/java.nix
+++ nix/roles/java.nix
@@ -1,27 +1,7 @@
-{ pkgs, config, system, ... }:
+{ pkgs, pkgs-unstable, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    temurin-bin-21
+  environment.systemPackages = [
+    pkgs-unstable.temurin-bin-21
   ];
-
-  environment.variables.JAVAX_NET_SSL_TRUSTSTORE =
-    let
-      caBundle = config.environment.etc."ssl/certs/ca-bundle.crt".source;
-      p11kit = pkgs.p11-kit.overrideAttrs (oldAttrs: {
-        configureFlags = [
-          "--with-trust-paths=${caBundle}"
-        ];
-      });
-    in derivation {
-      name = "java-cacerts";
-      builder = pkgs.writeShellScript "java-cacerts-builder" ''
-        ${p11kit.bin}/bin/trust \
-          extract \
-          --format=java-cacerts \
-          --purpose=server-auth \
-          $out
-      '';
-      system = system;
-    };
 }
