1acc1f5f587261cc707a4c4f190a0498a77e04d9
Philipp Heuer <git@philippheuer.me>
2024-01-20 20:06:16 +0100

    feat(nix): tweak default firefox config, prefer-dark-theme in gtk config

--- nix/roles/firefox.nix
+++ nix/roles/firefox.nix
@@ -3,14 +3,48 @@
 {
   programs.firefox = {
     enable = true;
-    package = pkgs-unstable.firefox-bin;
+    package = pkgs-unstable.wrapFirefox pkgs-unstable.firefox-unwrapped {
+      extraPolicies = {
+        # telemetry
+        DisableTelemetry = true;
+
+        # setup
+        UserMessaging = {
+          ExtensionRecommendations = false;
+          SkipOnboarding = true;
+        };
+
+        # disable captive portal detection
+        CaptivePortal = false;
+
+        # disable other unwanted features
+        DisableFirefoxStudies = true;
+        DisablePocket = true;
+        FirefoxHome = {
+          Pocket = false;
+          Snippets = false;
+        };
+
+        # enable importing system certificates (/usr/lib64/mozilla/certificates)
+        ImportEnterpriseRoots = true;
+
+        # extension settings
+        ExtensionSettings = {};
+      };
+    };
     preferences = {
+      # green https icon
+      "security.secure_connection_icon_color_gray" = false;
+
+      # file picker
       "widget.use-xdg-desktop-portal.file-picker" = 1;
     };
   };
 
   environment.sessionVariables = {
+    MOZ_ENABLE_WAYLAND = "1"; # wayland support
     MOZ_USE_XINPUT2 = "1"; # improved touchpad scrolling
+    MOZ_DBUS_REMOTE = "1"; # dbus
   };
 
   # set firefox as the default browser
