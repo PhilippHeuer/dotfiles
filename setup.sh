855c3f114057ac449815649b0aedccc3951bb216
Philipp Heuer <git@philippheuer.me>
2024-01-22 20:27:37 +0100

    feat(sh): add perms helper cmd to justfile

--- setup.sh
+++ setup.sh
@@ -3,9 +3,6 @@
 # properties
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# restore permissions
-chmod +x "$script_dir"/bin/* "$script_dir"/**/*.sh
-
 # install dotfiles
 export DOTFILE_THEME="catppuccin-mocha"
 dotfiles install "$script_dir" --mode symlink
