7c63510fd6754c60c3b846e0f8667597ed177ca3
Philipp Heuer <git@philippheuer.me>
2024-02-01 18:14:01 +0100

    feat(nix): simplify proxy configuration

--- nix/nixos/proxy.nix
+++ nix/nixos/proxy.nix
@@ -1,11 +1,39 @@
 # proxy
 { ... }:
 
-{
-  networking = {
-    proxy = {
-      #default = "";
-      #noProxy = "127.0.0.1,localhost,::1,*.local";
-    };
+let
+  # config
+  proxy = {
+    enable = false;
+    https = "https://localhost:3128";
+    noProxy = "127.0.0.1,::1,localhost,.local";
   };
+
+  proxySettings = if proxy.enable then {
+    # system proxy
+    proxy = {
+      default = proxy.https;
+      noProxy = proxy.noProxy;
+    };
+
+    # environment variables for applications that ignore the system proxy
+    environment = {
+      # electron-based applications
+      ELECTRON_GET_USE_PROXY = "true";
+      GLOBAL_AGENT_HTTPS_PROXY = proxy.https;
+    };
+  } else {
+    # defaults
+    proxy = {
+      default = "";
+      noProxy = "";
+    };
+    environment = {};
+  };
+in {
+  # apply proxy settings
+  networking.proxy = proxySettings.proxy;
+
+  # apply environment variables
+  environment.variables = proxySettings.environment;
 }
