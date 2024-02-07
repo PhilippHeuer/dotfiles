99830adcaed8ec7920f93d1141ab0665f558a478
Philipp Heuer <git@philippheuer.me>
2024-02-08 00:38:33 +0100

    feat(nix): install requests python pkg globally

--- nix/roles/python.nix
+++ nix/roles/python.nix
@@ -7,6 +7,7 @@
       python311.withPackages(ps: with ps; [
         pyyaml # some of the python scripts require yaml
         inquirer # select ui for python scripts
+        requests # http requests
       ]
     ))
   ];
