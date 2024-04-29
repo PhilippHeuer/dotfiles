27f2b039d4f75419521ec1359010b4faa96eff9c
Philipp Heuer <git@philippheuer.me>
2024-04-30 01:03:47 +0200

    fix(nix): install idea-community via bin pkg, src is outdated

--- nix/roles/idea-community.nix
+++ nix/roles/idea-community.nix
@@ -6,9 +6,8 @@
   # plugins: https://plugins.jetbrains.com/
   # json: https://github.com/NixOS/nixpkgs/blob/master/pkgs/applications/editors/jetbrains/plugins/plugins.json
   environment.systemPackages = with pkgs-unstable; [
-    (jetbrains.plugins.addPlugins jetbrains.idea-community [
+    (jetbrains.plugins.addPlugins jetbrains.idea-community-bin [
       "github-copilot" # https://plugins.jetbrains.com/plugin/17718-github-copilot
     ])
   ];
-  
 }
