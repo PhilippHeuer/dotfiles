63afce70baacae227c5272bb3e1408b0bbaa81c2
Philipp Heuer <git@philippheuer.me>
2024-05-04 02:05:54 +0200

    chore(nvim): bump lockfile, ensure systemd-user service starts

--- config/nvim/lua/philippheuer/plugins/neorg.lua
+++ config/nvim/lua/philippheuer/plugins/neorg.lua
@@ -1,10 +1,11 @@
 return {
   {
     'nvim-neorg/neorg',
-    build = ':Neorg sync-parsers',
+    --build = ':Neorg sync-parsers',
     lazy = true, -- enable lazy load
     ft = 'norg', -- lazy load on file type
     cmd = 'Neorg', -- lazy load on command
+    version = 'v7.0.0',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       require('neorg').setup {
