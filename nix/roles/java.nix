b8ba2a7b3e491d2e87800671633cf7894a17cf85
Philipp Heuer <git@philippheuer.me>
2024-05-12 14:10:22 +0200

    feat(nix): install multiple jdk versions

--- nix/roles/java.nix
+++ nix/roles/java.nix
@@ -1,7 +1,19 @@
 { pkgs, pkgs-unstable, ... }:
 
-{
+let
+  jdk21 = pkgs-unstable.jdk21;
+  jdk17 = pkgs-unstable.jdk17;
+  jdk11 = pkgs-unstable.jdk11;
+in {
   environment.systemPackages = [
-    pkgs-unstable.jdk21
+    jdk21
+    jdk17
+    jdk11
   ];
+
+  environment.variables = {
+    JAVA_21_HOME = jdk21.outPath;
+    JAVA_17_HOME = jdk17.outPath;
+    JAVA_11_HOME = jdk11.outPath;
+  };
 }
