7652b90154e37d6358b8187ddcddfb1a6eb27d57
Philipp Heuer <git@philippheuer.me>
2024-05-20 14:55:57 +0200

    feat(vscode): add excalidraw, fzf file finder and a few others

--- nix/roles/ide-vscode.nix
+++ nix/roles/ide-vscode.nix
@@ -11,23 +11,18 @@ in {
       vscode = vscodium;
       vscodeExtensions = [
         # general
-        extensions.vscode-marketplace.editorconfig.editorconfig
+        extensions.vscode-marketplace.editorconfig.editorconfig # editorconfig
+        extensions.vscode-marketplace.eamodio.gitlens # git info
+        extensions.vscode-marketplace.oderwat.indent-rainbow # colorize indentation
+        extensions.vscode-marketplace.wayou.vscode-todo-highlight # highlight todos
+        
+        # productivity
+        extensions.vscode-marketplace.vscodevim.vim # vim hotkeys
+        extensions.vscode-marketplace.gruntfuggly.todo-tree # todo tree
+        extensions.vscode-marketplace.rlivings39.fzf-quick-open
 
-        # ai
-        extensions.vscode-marketplace.github.copilot
-        extensions.vscode-marketplace.github.copilot-chat
-
-        # hotkeys
-        extensions.vscode-marketplace.vscodevim.vim
-
-        # git
-        extensions.vscode-marketplace.eamodio.gitlens
-
-        # ident rainbow
-        extensions.vscode-marketplace.oderwat.indent-rainbow
-
-        # todo highlight
-        extensions.vscode-marketplace.wayou.vscode-todo-highlight
+        # whiteboard
+        extensions.vscode-marketplace.pomdtr.excalidraw-editor
 
         # lsp
         extensions.vscode-marketplace.ms-python.python # python
@@ -37,12 +32,19 @@ in {
         extensions.vscode-marketplace.tamasfe.even-better-toml # toml
         extensions.vscode-marketplace.redhat.vscode-yaml # yaml
 
+        # auto completion
+        extensions.vscode-marketplace.github.copilot
+        extensions.vscode-marketplace.github.copilot-chat
+
         # theme
         extensions.vscode-marketplace.catppuccin.catppuccin-vsc
         extensions.vscode-marketplace.catppuccin.catppuccin-vsc-icons
         extensions.vscode-marketplace.enkia.tokyo-night
         extensions.vscode-marketplace.arcticicestudio.nord-visual-studio-code
         extensions.vscode-marketplace.mvllow.rose-pine
+
+        # other
+        extensions.vscode-marketplace.adpyke.codesnap # code screenshots
       ];
     })
   ];
