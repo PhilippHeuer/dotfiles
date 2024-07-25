426fee51fbff663f96ab4e94b72c1e92639cda00
Philipp Heuer <git@philippheuer.me>
2024-07-25 17:54:57 +0200

    chore(nvim): update which-key config for v3

--- config/nvim/lua/philippheuer/plugins/whichkey.lua
+++ config/nvim/lua/philippheuer/plugins/whichkey.lua
@@ -11,16 +11,18 @@ return {
     },
     config = function()
       -- [[ document command categories ]]
-      require('which-key').register {
-        ['<leader>u'] = { name = '[U]I', _ = 'which_key_ignore' },
-        ['<leader>l'] = { name = '[L]ayout', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = '[H]arpoon', _ = 'which_key_ignore' },
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+      require('which-key').add {
+        { '<leader>c', group = '[C]ode' },
+        { '<leader>d', group = '[D]ocument' },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>w', group = '[W]orkspace' },
+        { '<leader>t', group = '[T]oggle' },
+        -- { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        { '<leader>g', group = '[G]it' },
+        { '<leader>u', group = '[U]I' },
+        { '<leader>l', group = '[L]ayout' },
+        { '<leader>h', group = '[H]arpoon'},
       }
     end,
   },
