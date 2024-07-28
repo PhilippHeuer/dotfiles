78b3c471a571ead3ae2c5f28842bcb4b5ab9400c
Philipp Heuer <git@philippheuer.me>
2024-07-28 22:56:58 +0200

    feat(nix): add crash check loop to waybar start script, hyprland launcher script issues

--- local/scripts/waybar.sh
+++ local/scripts/waybar.sh
@@ -6,15 +6,34 @@
 # supported window managers:
 # - sway
 # - hyprland
-#
-# special cases:
-# - on wsl this clsoes the current session directly
 ###
-
 PIDFILE="/tmp/waybar.pid"
 
 # common functions
 source ~/.local/scripts/common.sh
+
+# function to start waybar
+start_waybar() {
+    WM=$(detect_wm)
+    WSL=$(is_wsl)
+    CASE="$WM$([ "$WSL" = true ] && echo "-wsl")"
+    echo "Starting waybar for $CASE"
+    case "$CASE" in
+        sway)
+            waybar -c ~/.config/waybar/sway/config.jsonc -s ~/.config/waybar/sway/style.css &
+            ;;
+        sway-wsl)
+            waybar -c ~/.config/waybar/sway/config-wsl.jsonc -s ~/.config/waybar/sway/style.css &
+            ;;
+        hyprland)
+            waybar -c ~/.config/waybar/hyprland/config.jsonc -s ~/.config/waybar/hyprland/style.css &
+            ;;
+        hyprland-wsl)
+            waybar -c ~/.config/waybar/hyprland/config-wsl.jsonc -s ~/.config/waybar/hyprland/style.css &
+            ;;
+    esac
+    echo $! > "$PIDFILE"
+}
 
 # check if waybar is running
 if [ -e "$PIDFILE" ] && ps -p $(cat "$PIDFILE") &> /dev/null; then
@@ -22,27 +41,16 @@ if [ -e "$PIDFILE" ] && ps -p $(cat "$PIDFILE") &> /dev/null; then
     kill -9 "$(cat "$PIDFILE")"
 fi
 
-# start waybar based on window manager and wsl status
-WM=$(detect_wm)
-WSL=$(is_wsl)
-CASE="$WM$([ "$WSL" = true ] && echo "-wsl")"
-echo "Starting waybar for $CASE"
-case "$CASE" in
-    sway)
-        waybar -c ~/.config/waybar/sway/config.jsonc -s ~/.config/waybar/sway/style.css &
-        ;;
-    sway-wsl)
-        waybar -c ~/.config/waybar/sway/config-wsl.jsonc -s ~/.config/waybar/sway/style.css &
-        ;;
-    hyprland)
-        waybar -c ~/.config/waybar/hyprland/config.jsonc -s ~/.config/waybar/hyprland/style.css &
-        ;;
-    hyprland-wsl)
-        waybar -c ~/.config/waybar/hyprland/config-wsl.jsonc -s ~/.config/waybar/hyprland/style.css &
-        ;;
-    *)
-        echo "Error: waybar for window manager ($WM) not supported. Exiting."
-        exit 1
-        ;;
-esac
-echo $! > "$PIDFILE"
+# start waybar initially
+start_waybar
+
+# check for loop argument
+if [ "$1" = "loop" ]; then
+    while true; do
+        sleep 5
+        if ! ps -p $(cat "$PIDFILE") &> /dev/null; then
+            echo "Waybar has crashed. Restarting..."
+            start_waybar
+        fi
+    done
+fi
