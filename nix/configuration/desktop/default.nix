d8c7fecd11c6a554b6ec4ce9bc13d6b08dd49a99
Philipp Heuer <git@philippheuer.me>
2024-07-29 00:21:52 +0200

    feat(nix): use rofi-wayland, fullscreen keybind, dedicated monitor config

--- nix/configuration/desktop/default.nix
+++ nix/configuration/desktop/default.nix
@@ -42,4 +42,28 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+
+  # Display configuration
+  environment.etc = {
+    "hyprland-display.conf" = {
+      text = ''
+        monitor=HDMI-A-1, 1920x1080@60, 0x0, 1
+        monitor=DP-3, 3440x1440@99.98, 1920x0, 1
+
+        workspace=11,monitor:HDMI-A-1
+        workspace=1,monitor:DP-3
+        workspace=2,monitor:DP-3
+        workspace=3,monitor:DP-3
+        workspace=4,monitor:DP-3
+        workspace=5,monitor:DP-3
+        workspace=6,monitor:DP-3
+        workspace=7,monitor:DP-3
+        workspace=8,monitor:DP-3
+        workspace=9,monitor:DP-3
+        workspace=10,monitor:DP-3
+        
+	      exec-once=xrandr --output DP-3 --primary
+      '';
+    };
+  };
 }
