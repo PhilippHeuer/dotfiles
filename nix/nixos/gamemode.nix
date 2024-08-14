a0943de0e050d1e434ea153666284085b5ce7b12
Philipp Heuer <git@philippheuer.me>
2024-08-14 14:34:43 +0200

    feat(nix): add gamemode config

---
+++ nix/nixos/gamemode.nix
@@ -0,0 +1,17 @@
+{ pkgs, lib, ... }:
+
+{
+  programs.gamemode = {
+    enable = true;
+    settings = {
+      general = {
+        renice = 10;
+      };
+
+      custom = {
+        start = "${pkgs.libnotify}/bin/notify-send 'GameMode started'";
+        end = "${pkgs.libnotify}/bin/notify-send 'GameMode ended'";
+      };
+    };
+  };
+}
