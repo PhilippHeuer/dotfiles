40f04269a7387a70b2beae2d831b1e0f75684515
Philipp Heuer <git@philippheuer.me>
2024-08-18 03:52:47 +0200

    feat(sh): use template_files in dotfiles config to set the font

---
+++ local/scripts/actions/gtktheme.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark > /dev/null 2>&1 || true
+gsettings set org.gnome.desktop.interface gtk-theme {{ .GTKTheme }} > /dev/null 2>&1 || true
+gsettings set org.gnome.desktop.interface icon-theme {{ .IconTheme }} > /dev/null 2>&1 || true
+gsettings set org.gnome.desktop.interface cursor-theme {{ .CursorTheme }} > /dev/null 2>&1 || true
