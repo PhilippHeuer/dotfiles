03615ca202a5a79ae5f4f720fffabbf4773d8ea9
Philipp Heuer <git@philippheuer.me>
2024-08-22 23:24:47 +0200

    chore(sh): make script files executable in git index

--- setup.sh
+++ setup.sh
@@ -4,11 +4,16 @@
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # fix permissions
-find "$script_dir/local/bin" -type f -exec chmod +x {} \;
-find "$script_dir/local/scripts" -type f -exec chmod +x {} \;
+chmod +x "$script_dir/setup.sh"
+find "$script_dir/local/bin" -type f -exec sh -c 'chmod +x "$1" && git update-index --chmod=+x "$1"' _ {} \;
+find "$script_dir/local/scripts" -type f -exec sh -c 'chmod +x "$1" && git update-index --chmod=+x "$1"' _ {} \;
 
 # install dotfiles
 dotfiles install "$script_dir" --mode symlink
+
+# clean up broken symlinks
+# find -L ~/.config -maxdepth 3 -type l -delete
+find -L ~/.local/share/backgrounds -maxdepth 3 -type l -delete
 
 # rebuild bat cache
 # bat cache --build
