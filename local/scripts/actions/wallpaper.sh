7b9f9d931e49fd93ea4799ff992c2345797a0838
Philipp Heuer <git@philippheuer.me>
2024-09-07 00:13:22 +0200

    chore(deps): lock file maintenance

--- local/scripts/actions/wallpaper.sh
+++ local/scripts/actions/wallpaper.sh
@@ -6,19 +6,19 @@ WAYPAPER_ARGS="--state-file ~/.local/state/waypaper/${THEME}.ini"
 
 # show gui
 show_gui() {
-    nohup waypaper --folder $(dotfiles query wallpaperdir) $WAYPAPER_ARGS &
+  waypaper --folder $(dotfiles query wallpaperdir) $WAYPAPER_ARGS
 }
 
 # set random wallpaper
 set_random() {
-    nohup waypaper --folder $(dotfiles query wallpaperdir) $WAYPAPER_ARGS --random &
+    waypaper --folder $(dotfiles query wallpaperdir) $WAYPAPER_ARGS --random
 }
 
 # restore last wallpaper
 restore() {
     if [ -f ~/.local/state/waypaper/${THEME}.ini ]; then
         echo "Restoring waypaper state from ~/.local/state/waypaper/${THEME}.ini"
-        nohup waypaper --folder $(dotfiles query wallpaperdir) $WAYPAPER_ARGS --restore &
+        waypaper --folder $(dotfiles query wallpaperdir) $WAYPAPER_ARGS --restore
     else
         echo "No waypaper state found, setting random wallpaper"
         set_random
