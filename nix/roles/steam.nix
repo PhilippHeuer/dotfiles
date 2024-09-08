62dd337a9732324ede8e64e55f7e21fc37a445e4
Philipp Heuer <git@philippheuer.me>
2024-09-08 23:36:30 +0200

    feat(nix): tweak steam config, config file updates

--- nix/roles/steam.nix
+++ nix/roles/steam.nix
@@ -5,10 +5,22 @@
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
-  };
+    # https://www.reddit.com/r/NixOS/comments/1bcsud4/some_steam_games_will_not_launch_on_nixos_but_on/
+    package = pkgs.steam.override {
+      extraPkgs = (pkgs: with pkgs; [
+        pango
+        harfbuzz
 
-  # packages
-  environment.systemPackages = with pkgs; [
-    steam
-  ];
+        xorg.libXcursor xorg.libXi xorg.libXinerama xorg.libXScrnSaver
+        libpng libpulseaudio libvorbis stdenv.cc.cc.lib
+        libkrb5 keyutils
+
+        mangohud # system stats
+      ]);
+      extraEnv = {
+        MANGOHUD = true;
+        OBS_VKCAPTURE = true;
+      };
+    };
+  };
 }
