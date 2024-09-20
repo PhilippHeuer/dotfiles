fcf7e23a4092da0fb28b6cc86b75442fa8101713
Philipp Heuer <git@philippheuer.me>
2024-09-20 23:31:12 +0200

    feat(nix): add vault-token-helper

--- nix/roles/vault.nix
+++ nix/roles/vault.nix
@@ -1,7 +1,8 @@
-{ pkgs, pkgs-unstable, ... }:
+{ pkgs, pkgs-unstable, inputs, ... }:
 
 {
   environment.systemPackages = [
-    pkgs-unstable.vault
+    pkgs-unstable.vault # cli
+    inputs.nixpkgs-philippheuer.packages.${pkgs.system}.vault-token-helper # vault token helper
   ];
 }
