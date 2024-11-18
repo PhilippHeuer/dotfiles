baf0bc3f961b1129d64a4c3b0fa9c47e8adf686f
Philipp Heuer <git@philippheuer.me>
2024-11-18 17:39:05 +0100

    chore(deps): lock file maintenance

--- nix/roles/lang/nodejs.nix
+++ nix/roles/lang/nodejs.nix
@@ -1,7 +1,7 @@
-{ pkgs, ... }:
+{ pkgs, pkgs-unstable, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    nodejs_22
+  environment.systemPackages = [
+    pkgs-unstable.nodejs_23
   ];
 }
