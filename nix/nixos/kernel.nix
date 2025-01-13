ed3e2dafd2b56f85aa683261f67e3eb8ccf37450
Philipp Heuer <git@philippheuer.me>
2025-01-13 21:51:19 +0100

    chore(nix): lock file maintenance, update kernel settings

--- nix/nixos/kernel.nix
+++ nix/nixos/kernel.nix
@@ -14,11 +14,52 @@
     # supported filesystems
     supportedFilesystems = [ "ntfs" ];
 
-    # kernel parameters
-    kernelParams = lib.optionals config.boot.initrd.systemd.enable ["systemd.show_status=false"];
-
     # kernel packages
     kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
+
+    # kernel parameters
+    kernelParams = lib.optionals config.boot.initrd.systemd.enable [
+      #"systemd.show_status=false" # hide boot information
+      #"quiet" # hide splash screen
+      #"fbcon=nodefer" # disable framebuffer console
+
+      "preempt=full" # kernel preemption
+      #"vt.global_cursor_default=0"
+      #"kernel.modules_disabled=1"
+      #"lsm=landlock,lockdown,yama,integrity,apparmor,bpf,tomoyo,selinux"
+      #"usbcore.autosuspend=-1"
+      #"video4linux"
+      #"acpi_rev_override=5"
+      #"security=selinux"
+    ];
+
+    # kernel sysctl
+    kernel.sysctl = {
+      # performance
+      "kernel.sched_cfs_bandwidth_slice_us" = 3000;
+      "vm.max_map_count" = 2147483642;
+      "fs.inotify.max_user_instances" = 8192;
+      "fs.inotify.max_user_watches" = 524288;
+
+      # network
+      "net.ipv4.tcp_fin_timeout" = 5; # reduce TCP FIN timeout
+      #"net.ipv4.conf.all.forwarding" = 1;
+      #"net.ipv6.conf.all.forwarding" = 1;
+      #"net.ipv4.conf.all.accept_redirects" = 1;
+      #"net.ipv6.conf.all.accept_redirects" = 1;
+
+      # security
+      "kernel.dmesg_restrict" = 1; # restrict access to dmesg messages
+
+      # misc
+      "kernel.sysrq" = 1; # enable sysrq
+    };
+
+    #extraModprobeConfig = ''
+    #  options vfio_iommu_type1 allow_unsafe_interrupts=1
+    #  options kvm ignore_msrs=1
+    #  options kvm report_ignored_msrs=0
+    #'';
 
     # initrd systemd
     initrd.systemd.enable = lib.mkDefault true;
@@ -37,4 +78,10 @@
   environment.systemPackages = with pkgs; [
     policycoreutils # SELinux
   ];
+
+  # eBPF scheduling extensions (sched-ext)
+  #services.scx = {
+  #  enable = true;
+  #  scheduler = "sxc_lavd";
+  #};
 }
