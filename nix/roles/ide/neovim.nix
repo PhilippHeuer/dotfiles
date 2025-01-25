ec075bf5d2c83475ffba38225881342819e3f3f2
Philipp Heuer <git@philippheuer.de>
2025-01-25 22:39:49 +0100

    chore(deps): bump lockfiles

--- nix/roles/ide/neovim.nix
+++ nix/roles/ide/neovim.nix
@@ -10,9 +10,10 @@
     pkgs-unstable.nixd # nix
     pkgs-unstable.rust-analyzer # rust
     pkgs-unstable.jdt-language-server # java
-    pkgs-unstable.yaml-language-server # yaml
-    pkgs-unstable.bash-language-server # bash
+    pkgs.yaml-language-server # yaml
+    pkgs.bash-language-server # bash
     pkgs-unstable.ansible-language-server # ansible
+    pkgs.zls # zig
 
     # formatters
     pkgs-unstable.nixfmt-rfc-style
