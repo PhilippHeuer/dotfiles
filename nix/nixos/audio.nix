ec3a3887432b7c60bcc4347e88fe47972fbaff4b
Philipp Heuer <git@philippheuer.de>
2025-05-25 12:13:45 +0200

    chore(nix): fix java symlink creation

--- nix/nixos/audio.nix
+++ nix/nixos/audio.nix
@@ -2,7 +2,7 @@
 
 {
   # enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
--- nix/roles/lang/java.nix
+++ nix/roles/lang/java.nix
@@ -3,26 +3,29 @@
 let
   jdk21 = pkgs-unstable.jdk21;
   jdk17 = pkgs-unstable.jdk17;
-  jdk11 = pkgs-unstable.jdk11;
 in
 {
   environment.systemPackages = [
     jdk21
     jdk17
-    jdk11
   ];
 
   environment.variables = {
     JAVA_HOME_21 = jdk21.outPath;
     JAVA_HOME_17 = jdk17.outPath;
-    JAVA_HOME_11 = jdk11.outPath;
   };
 
   system.activationScripts.jdkSymlinks.text = ''
     mkdir -p /opt/java
 
-    ln -sf ${jdk21} /opt/java/jdk-21
-    ln -sf ${jdk17} /opt/java/jdk-17
-    ln -sf ${jdk11} /opt/java/jdk-11
+    if [ "$(readlink /opt/java/jdk-21)" != "${jdk21}/lib/openjdk" ]; then
+      rm /opt/java/jdk-21 || true
+      ln -s "${jdk21}/lib/openjdk" /opt/java/jdk-21
+    fi
+
+    if [ "$(readlink /opt/java/jdk-17)" != "${jdk17}/lib/openjdk" ]; then
+      rm /opt/java/jdk-17 || true
+      ln -s "${jdk17}/lib/openjdk" /opt/java/jdk-17
+    fi
   '';
 }
