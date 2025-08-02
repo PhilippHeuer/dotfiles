2faf998fe442aa27efce1ebfd6a2a43aa8440667
Philipp Heuer <git@philippheuer.de>
2025-08-03 00:40:40 +0200

    feat(nix): add host ahd

---
+++ nix/configuration/ahd/default.nix
@@ -0,0 +1,41 @@
+{
+  pkgs,
+  username,
+  lib,
+  ...
+}:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  # hardware acceleration
+  hardware.graphics.enable32Bit = true;
+  hardware.graphics.extraPackages = with pkgs; [
+    vaapiIntel
+    libvdpau-va-gl
+    vaapiVdpau
+    intel-ocl
+  ];
+  hardware.graphics.extraPackages32 = with pkgs.pkgsi686Linux; [
+    libva
+    vaapiIntel
+    libvdpau-va-gl
+    vaapiVdpau
+  ];
+
+  # shell
+  users.defaultUserShell = pkgs.bash;
+
+  # hostname
+  networking.hostName = "ahd";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+  networking.networkmanager.wifi.scanRandMacAddress = false; # causes issues with some wifi networks
+
+  # secrets
+  sops.age.keyFile = lib.mkForce "/home/${username}/.config/sops/age/keys.txt";
+  sops.defaultSopsFile = lib.mkForce ../../secrets/ahd.yaml;
+}
