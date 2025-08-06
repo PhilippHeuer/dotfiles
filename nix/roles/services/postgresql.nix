1831486a778bc15923ab9996e18886bf7d9cab8a
Philipp Heuer <git@philippheuer.de>
2025-08-06 23:44:54 +0200

    fix(nix): add psql auth config

--- nix/roles/services/postgresql.nix
+++ nix/roles/services/postgresql.nix
@@ -9,6 +9,11 @@
     extensions = [
       pkgs.postgresql17Packages.pgvector
     ];
+    authentication = ''
+      local all all trust
+      host all all 0.0.0.0/0 scram-sha-256
+      host all all ::/0 scram-sha-256
+    '';
   };
 
   # firewall for local access
