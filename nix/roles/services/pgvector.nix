419462249b4cc75a4e3da9ea5bb7c063fd8f97a1
Philipp Heuer <git@philippheuer.de>
2025-08-06 21:39:12 +0200

    feat(nix): add test pg db, openwebui

---
+++ nix/roles/services/pgvector.nix
@@ -0,0 +1,18 @@
+{ pkgs, lib, ... }:
+
+{
+  # systemd container
+  virtualisation.oci-containers.containers = {
+    pgvector = {
+      image = "docker.io/pgvector/pgvector:0.8.0-pg17";
+      autoStart = true;
+      ports = [ "5432:5432/tcp" ];
+      volumes = [
+        "/var/lib/pgvector:/var/lib/postgresql/data"
+      ];
+    };
+  };
+
+  # firewall for local access
+  networking.firewall.allowedTCPPorts = [ 5432 ];
+}
