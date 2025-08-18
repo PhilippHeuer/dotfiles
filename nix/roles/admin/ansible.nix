868cf59f4e65b733a1d221565915b63df949a6c1
Philipp Heuer <git@philippheuer.de>
2025-08-18 16:53:28 +0200

    feat(nix): update lockfile, add ansible-lint

--- nix/roles/admin/ansible.nix
+++ nix/roles/admin/ansible.nix
@@ -1,7 +1,16 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    ansible
+  environment.systemPackages = [
+    pkgs.ansible
+    pkgs.ansible-lint
+    /*
+    (pkgs.python313.withPackages (pp: [
+      pp.ansible
+      pp.ansible-core
+      pkgs.ansible-lint
+      pp.pywinrm # Windows Remote Management
+    ]))
+    */
   ];
 }
