bb6eeeb999faeca6b19da1ef6f497ab07d0f70aa
Philipp Heuer <git@philippheuer.de>
2025-09-03 22:04:17 +0200

    fix(nix): use qt6 sddm, add theme as extraPackage

--- nix/nixos/de-sddm.nix
+++ nix/nixos/de-sddm.nix
@@ -2,6 +2,7 @@
   pkgs,
   pkgs-unstable,
   inputs,
+  lib,
   ...
 }:
 
@@ -12,33 +13,35 @@
     theme = "sddm-astronaut-theme";
     settings.Theme.CursorTheme = "Bibata-Modern-Ice";
     wayland.enable = true;
+    package = lib.mkForce pkgs.kdePackages.sddm; # qt6 sddm version
+    extraPackages = [
+      # sddm theme
+      (pkgs.sddm-astronaut.override {
+        themeConfig = {
+          # [Background]
+          CustomBackground = "true";
+          Background = "${inputs.nixpkgs-philippheuer.packages.${pkgs.system}.wallpapers}/share/backgrounds/catppuccin-mocha/nice-blue-background.png";
+
+          # [Blur Settings]
+          FullBlur = "false";
+          PartialBlur = "true";
+          BlurRadius = "80";
+
+          # [Design Customizations]
+          FormPosition = "left";
+          Font = "JetBrainsMono Nerd Font Mono";
+          FontSize = "9";
+
+          # [Interface Behavior]
+          ForceHideVirtualKeyboardButton = "true";
+        };
+      })
+    ];
   };
 
   # sddm themes
   environment.systemPackages = [
     # wallpapers
     inputs.nixpkgs-philippheuer.packages.${pkgs.system}.wallpapers
-
-    # sddm theme
-    (pkgs.sddm-astronaut.override {
-      themeConfig = {
-        # [Background]
-        CustomBackground = "true";
-        Background = "${inputs.nixpkgs-philippheuer.packages.${pkgs.system}.wallpapers}/share/backgrounds/catppuccin-mocha/nice-blue-background.png";
-
-        # [Blur Settings]
-        FullBlur = "false";
-        PartialBlur = "true";
-        BlurRadius = "80";
-
-        # [Design Customizations]
-        FormPosition = "left";
-        Font = "JetBrainsMono Nerd Font Mono";
-        FontSize = "9";
-
-        # [Interface Behavior]
-        ForceHideVirtualKeyboardButton = "true";
-      };
-    })
   ];
 }
