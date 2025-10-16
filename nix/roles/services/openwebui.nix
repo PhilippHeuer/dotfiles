6d025782fec5e4a2d2f57c2f4d7208404c5e2207
Philipp Heuer <git@philippheuer.de>
2025-10-16 23:47:48 +0200

    chore(nix): bump lockfiles

--- nix/roles/services/openwebui.nix
+++ nix/roles/services/openwebui.nix
@@ -4,7 +4,7 @@
   # systemd container
   virtualisation.oci-containers.containers = {
     openwebui = {
-      image = "ghcr.io/open-webui/open-webui:v0.6.33";
+      image = "ghcr.io/open-webui/open-webui:v0.6.34";
       autoStart = true;
       ports = [ "3000:8080/tcp" ];
       volumes = [
