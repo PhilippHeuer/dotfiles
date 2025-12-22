e894d30e49d625bc73d3f8b74c19321498ba124e
Philipp Heuer <git@philippheuer.de>
2025-12-22 20:23:39 +0100

    feat(nix): add watchstate

---
+++ nix/roles/mediaserver/watchstate.nix
@@ -0,0 +1,35 @@
+{ pkgs, lib, ... }:
+
+let
+  # renovate: datasource=docker depName=ghcr.io/arabcoders/watchstate
+  version = "v1.0.3";
+in
+{
+  # systemd container
+  virtualisation.oci-containers.containers = {
+    watchstate = {
+      image = "ghcr.io/arabcoders/watchstate:" + version;
+      autoStart = true;
+      ports = [ "25699:8080/tcp" ];
+      user = "0:0";
+      volumes = [
+        "/var/lib/watchstate/config:/config"
+      ];
+    };
+  };
+
+  services.traefik.dynamicConfigOptions.http.routers.watchstate = {
+    rule = lib.mkDefault "Host(`watchstate.home`)";
+    service = "watchstate";
+    entrypoints = [ "web" "websecure" ];
+  };
+  services.traefik.dynamicConfigOptions.http.services.watchstate = {
+    loadBalancer = {
+      servers = [
+        {
+          url = "http://localhost:25699";
+        }
+      ];
+    };
+  };
+}
