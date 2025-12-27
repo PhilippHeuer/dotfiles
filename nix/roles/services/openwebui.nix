2d25067e27c16d6d81ea4cdc3e2ce32df5323c12
Philipp Heuer <git@philippheuer.de>
2025-12-27 14:30:59 +0100

    chore: update openwebui to v0.6.43

--- nix/roles/services/openwebui.nix
+++ nix/roles/services/openwebui.nix
@@ -4,7 +4,7 @@
   # systemd container
   virtualisation.oci-containers.containers = {
     openwebui = {
-      image = "ghcr.io/open-webui/open-webui:v0.6.41";
+      image = "ghcr.io/open-webui/open-webui:v0.6.43";
       autoStart = true;
       ports = [ "3000:8080/tcp" ];
       volumes = [
