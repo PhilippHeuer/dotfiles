a99dc17a1ceb6cade7ffc6049d70cf3d0d2c5aa6
Philipp Heuer <git@philippheuer.de>
2026-02-24 19:43:33 +0100

    chore(nix): update openwebui to v0.8.5

--- nix/roles/services/openwebui.nix
+++ nix/roles/services/openwebui.nix
@@ -4,12 +4,17 @@
   # systemd container
   virtualisation.oci-containers.containers = {
     openwebui = {
-      image = "ghcr.io/open-webui/open-webui:v0.7.2";
+      image = "ghcr.io/open-webui/open-webui:v0.8.5";
       autoStart = true;
       ports = [ "3000:8080/tcp" ];
       volumes = [
         "/var/lib/openwebui:/app/backend/data"
       ];
+      environment = {
+        ENABLE_VERSION_UPDATE_CHECK = "false";
+        ENABLE_FORWARD_USER_INFO_HEADERS = "true";
+        ENABLE_API_KEYS = "true";
+      };
     };
   };
 
