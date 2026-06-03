26127fb9d2ab39aaab3f77fdb1eed9ca9cd810a1
Philipp Heuer <git@philippheuer.de>
2026-06-03 21:56:49 +0200

    chore(nix): bump flake.lock

--- nix/roles/development/openapi.nix
+++ nix/roles/development/openapi.nix
@@ -10,6 +10,7 @@
     inputs.nixpkgs-philippheuer.packages.${pkgs.system}.openapi-changes
     inputs.nixpkgs-philippheuer.packages.${pkgs.system}.vacuum
     inputs.nixpkgs-philippheuer.packages.${pkgs.system}.oasdiff
+    inputs.nixpkgs-philippheuer.packages.${pkgs.system}.printing-press
     pkgs.redocly
   ];
 }
